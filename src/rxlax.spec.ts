ff34b27ee3c73ca33ae8d6c8040fcb186d36fbfe
greguz <gregolettogiacomo@gmail.com>
2019-01-06 18:19:48 +0100

    Improve tests

--- src/rxlax.spec.ts
+++ src/rxlax.spec.ts
@@ -1,16 +1,34 @@
 import "mocha";
 import { expect } from "chai";
 
-import { from } from "rxjs";
+import { from, Observable } from "rxjs";
 import { mergeAll } from "rxjs/operators";
 import { fill } from "lodash";
 
 import { rxlax } from "./index";
 
 describe("rxlax", () => {
+  function wait(ms: number) {
+    return function mapper<T>(data: T) {
+      return new Promise<T>(resolve => {
+        setTimeout(() => {
+          resolve(data);
+        }, ms);
+      });
+    };
+  }
+
+  it("should validate arguments", () => {
+    expect(() => rxlax({} as any)).to.throw();
+
+    expect(() => rxlax(wait(0), { concurrency: "0" as any })).to.throw();
+    expect(() => rxlax(wait(0), { concurrency: 1.2 })).to.throw();
+    expect(() => rxlax(wait(0), { concurrency: -1 })).to.throw();
+    expect(() => rxlax(wait(0), { concurrency: 0 })).to.throw();
+  });
+
   it("should work", async () => {
     const length = 100;
-
     const concurrency = 10;
 
     let calls = 0;
@@ -41,5 +59,84 @@ describe("rxlax", () => {
       .toPromise();
 
     expect(calls).to.be.equal(length);
+  });
+
+  it("should handle job errors", done => {
+    from([0])
+      .pipe(rxlax(data => Promise.reject(new Error("STOP"))))
+      .pipe(mergeAll())
+      .subscribe(
+        undefined,
+        error => {
+          if (error instanceof Error && error.message === "STOP") {
+            done();
+          } else {
+            done(error);
+          }
+        },
+        () => {
+          done(new Error("Done without errors"));
+        }
+      );
+  });
+
+  it("should handle observable errors", done => {
+    const observable = new Observable<string>(subscriber => {
+      subscriber.next("a");
+      subscriber.next("b");
+      subscriber.next("c");
+      subscriber.error(new Error("STOP"));
+    });
+
+    observable
+      .pipe(rxlax(wait(10), { concurrency: 1 }))
+      .pipe(mergeAll())
+      .subscribe(
+        undefined,
+        error => {
+          if (error instanceof Error && error.message === "STOP") {
+            done();
+          } else {
+            done(error);
+          }
+        },
+        () => {
+          done(new Error("Done without errors"));
+        }
+      );
+  });
+
+  it("should work with empty observables", done => {
+    const observable = new Observable<string>(subscriber => {
+      subscriber.complete();
+    });
+
+    observable
+      .pipe(rxlax(wait(10), { concurrency: 1 }))
+      .pipe(mergeAll())
+      .subscribe(undefined, done, done);
+  });
+
+  it("should work with empty observables", done => {
+    const observable = new Observable<string>(subscriber => {
+      subscriber.error(new Error("STOP"));
+    });
+
+    observable
+      .pipe(rxlax(wait(10), { concurrency: 1 }))
+      .pipe(mergeAll())
+      .subscribe(
+        undefined,
+        error => {
+          if (error instanceof Error && error.message === "STOP") {
+            done();
+          } else {
+            done(error);
+          }
+        },
+        () => {
+          done(new Error("Done without errors"));
+        }
+      );
   });
 });
