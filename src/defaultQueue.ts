63a79462ad2e6c977fb0eab15106eae7364e1f53
greguz <gregolettogiacomo@gmail.com>
2019-01-09 13:53:33 +0100

    Remove async/await usage

--- src/defaultQueue.ts
+++ src/defaultQueue.ts
@@ -3,15 +3,23 @@ import { Queue } from "./Queue";
 export function defaultQueue<T>(): Queue<T> {
   const store: T[] = [];
 
+  function shift() {
+    return Promise.resolve(store.shift());
+  }
+
+  function push(entry: T) {
+    store.push(entry);
+    return Promise.resolve();
+  }
+
+  function clear() {
+    store.splice(0, store.length);
+    return Promise.resolve();
+  }
+
   return {
-    async shift() {
-      return store.shift();
-    },
-    async push(entry: T) {
-      store.push(entry);
-    },
-    async clear() {
-      store.splice(0, store.length);
-    }
+    shift,
+    push,
+    clear
   };
 }
